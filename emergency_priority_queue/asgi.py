7fb95534d45af9a759f4e77e91ab154321b51248
Eric Silva <ericsilvap13@gmail.com>
2025-11-03 16:48:24 -0300

    chore: add environment variables config and static and media for settings and urls.py

--- emergency_priority_queue/asgi.py
+++ emergency_priority_queue/asgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emergency_priority_queue.settings')
+load_dotenv()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emergency_priority_queue.settings")
 
 application = get_asgi_application()
