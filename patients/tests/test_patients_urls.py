7db6856c354d0366adfa5e07ca775d6f620f24a2
Eric Silva <ericsilvap13@gmail.com>
2025-11-05 00:24:13 -0300

    test: tests for patients create view and url

--- patients/tests/test_patients_urls.py
+++ patients/tests/test_patients_urls.py
@@ -5,4 +5,8 @@ from django.urls import reverse
 class PatientsUrlTest(SimpleTestCase):
     def test_patients_register_url_is_ok(self):
         url = reverse("patients:register")
-        self.assertEqual(url, "/ipacientes/cadastro/")
+        self.assertEqual(url, "/pacientes/cadastro/")
+
+    def test_patients_register_post_url_is_ok(self):
+        url = reverse("patients:create")
+        self.assertEqual(url, "/pacientes/criar/")
--- patients/tests/test_patients_views.py
+++ patients/tests/test_patients_views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.test.testcases import SimpleTestCase
 from django.urls import reverse, resolve
 from patients import views
@@ -7,3 +8,19 @@ class PatientsViewsTest(SimpleTestCase):
     def test_patients_register_view_is_ok(self):
         view = resolve(reverse("patients:register"))
         self.assertIs(view.func, views.register_patient)
+
+    def test_patients_register_post_view_is_ok(self):
+        view = resolve(reverse("patients:create"))
+        self.assertIs(view.func, views.register_patient_post)
+
+    def test_patients_register_post_view_raises_405_on_GET(self):
+        url = reverse("patients:create")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 405)
+
+    def test_patients_register_post_view_redirects_to_register(self):
+        url = reverse("patients:create")
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+        response = self.client.post(url, follow=True)
+        self.assertTemplateUsed(response, "patients/pages/patients_list.html")
