4c60611062580f86ef7fd7cad4a3b12247503cc2
Eric Silva <ericsilvap13@gmail.com>
2025-11-18 15:51:07 -0300

    style: changed the order of the fields and add fields placeholder

--- patients/forms.py
+++ patients/forms.py
@@ -9,27 +9,37 @@ class PatientForm(forms.ModelForm):
 
     name = forms.CharField(
         label="Nome",
+        widget=forms.TextInput(attrs={"placeholder": "Ex: João da Silva"}),
+        min_length=4,
     )
     cpf = forms.CharField(
         label="CPF",
+        widget=forms.NumberInput(attrs={"placeholder": "Ex: 111111111-11"}),
         max_length=11,
     )
     birth_date = forms.DateField(
         label="Data de Nascimento",
+        widget=forms.NumberInput(attrs={"placeholder": "Ex: DD/MM/AAAA"}),
     )
     sus_card = forms.CharField(
         label="Número do SUS",
-        max_length=14,
+        widget=forms.NumberInput(attrs={"placeholder": "Ex: 1234567891012345"}),
     )
     dad = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Ex: José da Silva"}),
         label="Nome do Pai",
         required=False,
     )
     mom = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Ex: Maria da Silva"}),
         label="Nome da Mãe",
         required=False,
     )
-    phone_number = forms.CharField(label="Telefone", required=False)
+    phone_number = forms.CharField(
+        label="Telefone",
+        widget=forms.NumberInput(attrs={"placeholder": "Ex: (99) 9 9999-9999"}),
+        required=False,
+    )
 
     gender = forms.ChoiceField(
         label="Gênero",
--- patients/models.py
+++ patients/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Patient(models.Model):
     name = models.CharField(max_length=65)
-    cpf = models.CharField(max_length=11)
-    birth_date = models.DateField()
-    gender = models.CharField()
-    sus_card = models.CharField(max_length=14, null=True, blank=True)
-    phone_number = models.CharField(max_length=11, null=True, blank=True)
-    blood_type = models.CharField(max_length=3, null=True, blank=True)
     dad = models.CharField(max_length=65, null=True, blank=True)
     mom = models.CharField(max_length=65, null=True, blank=True)
+    cpf = models.CharField(max_length=11)
+    birth_date = models.DateField()
+    sus_card = models.CharField(max_length=14, null=True, blank=True)
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
+    gender = models.CharField()
+    blood_type = models.CharField(max_length=3, null=True, blank=True)
